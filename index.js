10bd2feaeb8fbbcf85d23b3a72967d1c3d9b4b95
archi-taek <architaek@hotmail.com>
2020-07-01 13:24:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // console.log("Im working. Im JS. Im Beautiful. Im worth it");
 
-let a = 221;
+/*let a = 221;
 let b = a - 5;
 a = 4;
-console.log(b, a);
+console.log(b, a);*/
+
+/*String
+const what = "TAEK";*/
+
+/*Boolean
+const wat = true;
+console.log(wat);*/
+
+/*Number
+const wat = 55;*/
+
+/*Float
+const wat = 55.1*/
+
+/*const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+console.log(daysOfWeek[0]);*/
+
+const taekInfo = {
+  name: "TAEK",
+  age: 3000,
+  gender: "Male",
+  isHandsome: true,
+  favMovies: ["Her", "Matrix"],
+  favFoods: [
+    { name: "Kimchi", fatty: false },
+    { name: "Meat", fatty: true },
+  ],
+};
+console.log(taekInfo.favFoods[1].name);
